b11863a8bc012f349fb9425340dd77c6ed9c4d90
Mac Gaulin <gaulinmp+git@gmail.com>
2018-09-19 20:42:15 -0600

    Add volume buttons

--- baseapp/pages.py
+++ baseapp/pages.py
@@ -11,8 +11,17 @@ from .broadlink import blink
 
 @app.route('/')
 def home():
+    icons = [
+        ('volume_up', 'volume_up.svg' ), 
+        ('volume_down', 'volume_down.svg' ), 
+        ('sat-catv', 'chromecast.svg' ), 
+        ('stb', 'computer.svg' ), 
+        ('game', 'ps4.svg' ), 
+        ('video', 'switch.svg' ), 
+    ]
     return render_template('pages/index.html', 
-            data={'devices': blink.get_device_dict()})
+            data={'devices': blink.get_device_dict(),
+                  'icons': icons})
 
 @app.route('/send/<string:device>/<string:command>')
 def send(device, command):
