2084158f4319badba0d7441c918d1acc4956c6db
Mac Gaulin <gaulinmp+git@gmail.com>
2018-09-23 10:53:29 -0600

    Fix volume buttons

--- baseapp/pages.py
+++ baseapp/pages.py
@@ -11,17 +11,37 @@ from .broadlink import blink
 
 @app.route('/')
 def home():
-    icons = [
-        ('volume_up', 'volume_up.svg' ), 
-        ('volume_down', 'volume_down.svg' ), 
-        ('sat-catv', 'chromecast.svg' ), 
-        ('stb', 'computer.svg' ), 
-        ('game', 'ps4.svg' ), 
-        ('video', 'switch.svg' ), 
+    devices = [ 
+        # Outer list: sections
+        # Inner list: (device-name, command-name, icon-name)
+        [ # Section 1: Visio TV
+            ('vizio-tv', 'power', 'new_tv.svg'),
+            ('vizio-tv', 'volume_up', 'volume_up.svg'), 
+            ('vizio-tv', 'volume_down', 'volume_down.svg'),
+            ('sony-reciever', 'main_sat-catv', 'chromecast.svg'),
+            ('sony-reciever', 'main_stb', 'computer.svg'),
+            ('sony-reciever', 'main_game', 'ps4.svg'),
+            ('sony-reciever', 'main_video', 'switch.svg'),
+        ],
+        [  # Section 1: Samsung TV
+            ('samsung-tv', 'power', 'old_tv.svg'),
+            ('samsung-tv', 'volume_up', 'volume_up.svg'),
+            ('samsung-tv', 'volume_down', 'volume_down.svg'),
+            ('sony-reciever', 'zone2_sat-catv', 'chromecast.svg'),
+            ('sony-reciever', 'zone2_stb', 'computer.svg'),
+            ('sony-reciever', 'zone2_game', 'ps4.svg'),
+            ('sony-reciever', 'zone2_video', 'switch.svg'),
+        ],
+        [  # Section 1: Sony Receiver
+            ('sony-receiver', 'power', 'receiver.jpg'),
+            ('sony-receiver', 'volume_up', 'volume_up.svg'),
+            ('sony-receiver', 'volume_down', 'volume_down.svg'),
+        ]
     ]
     return render_template('pages/index.html', 
-            data={'devices': blink.get_device_dict(),
-                  'icons': icons})
+            data={'all_devices': blink.get_device_dict(),
+                  'devices': devices, 
+                  })
 
 @app.route('/send/<string:device>/<string:command>')
 def send(device, command):
