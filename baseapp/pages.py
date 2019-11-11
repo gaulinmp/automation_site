4fe645a500e9f1cae4cc44ec03c5ee1dab301fac
Mac Gaulin <gaulinmp@gmail.com>
2019-11-10 23:18:08 -0700

    Add TCL TV

--- baseapp/pages.py
+++ baseapp/pages.py
@@ -15,28 +15,32 @@ def home():
         # Outer list: sections
         # Inner list: (device-name, command-name, icon-name)
         [ # Section 1: Visio TV
-            ('vizio-tv', 'power', 'new_tv.svg'),
-            ('vizio-tv', 'volume_up', 'volume_up.svg'), 
-            ('vizio-tv', 'volume_down', 'volume_down.svg'),
-            ('sony-reciever', 'main_sat-catv', 'chromecast.svg'),
-            ('sony-reciever', 'main_stb', 'computer.svg'),
-            ('sony-reciever', 'main_game', 'ps4.svg'),
-            ('sony-reciever', 'main_video', 'switch.svg'),
+            ('tcl-tv', 'power', 'new_tv.svg'),
+            ('tcl-tv', 'volume_up', 'volume_up.svg'), 
+            ('tcl-tv', 'volume_down', 'volume_down.svg'),
+            #('vizio-tv', 'power', 'new_tv.svg'),
+            #('vizio-tv', 'volume_up', 'volume_up.svg'), 
+            #('vizio-tv', 'volume_down', 'volume_down.svg'),
+            #('sony-reciever', 'main_sat-catv', 'chromecast.svg'),
+            #('sony-reciever', 'main_stb', 'computer.svg'),
+            #('sony-reciever', 'main_game', 'ps4.svg'),
+            #('sony-reciever', 'main_video', 'switch.svg'),
         ],
         [  # Section 1: Samsung TV
-            ('samsung-tv', 'power', 'old_tv.svg'),
-            ('samsung-tv', 'volume_up', 'volume_up.svg'),
-            ('samsung-tv', 'volume_down', 'volume_down.svg'),
-            ('sony-reciever', 'zone2_sat-catv', 'chromecast.svg'),
-            ('sony-reciever', 'zone2_stb', 'computer.svg'),
-            ('sony-reciever', 'zone2_game', 'ps4.svg'),
-            ('sony-reciever', 'zone2_video', 'switch.svg'),
+            #('samsung-tv', 'power', 'old_tv.svg'),
+            ('tcl-tv', 'Netflix', 'netflix.svg'),
+            #('samsung-tv', 'volume_up', 'volume_up.svg'),
+            #('samsung-tv', 'volume_down', 'volume_down.svg'),
+            #('sony-reciever', 'zone2_sat-catv', 'chromecast.svg'),
+            #('sony-reciever', 'zone2_stb', 'computer.svg'),
+            #('sony-reciever', 'zone2_game', 'ps4.svg'),
+            #('sony-reciever', 'zone2_video', 'switch.svg'),
         ],
-        [  # Section 1: Sony Receiver
-            ('sony-receiver', 'power', 'receiver.jpg'),
-            ('sony-receiver', 'volume_up', 'volume_up.svg'),
-            ('sony-receiver', 'volume_down', 'volume_down.svg'),
-        ]
+        #[  # Section 1: Sony Receiver
+            #('sony-receiver', 'power', 'receiver.jpg'),
+            #('sony-receiver', 'volume_up', 'volume_up.svg'),
+            #('sony-receiver', 'volume_down', 'volume_down.svg'),
+        #]
     ]
     return render_template('pages/index.html', 
             data={'all_devices': blink.get_device_dict(),
