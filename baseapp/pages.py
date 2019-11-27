2956c2ff50545dfa78b84feee43c96b2e5fa4d91
Mac Gaulin <gaulinmp@gmail.com>
2019-11-26 18:37:53 -0700

    Add arrows and update front page

--- baseapp/pages.py
+++ baseapp/pages.py
@@ -16,8 +16,29 @@ def home():
         # Inner list: (device-name, command-name, icon-name)
         [ # Section 1: Visio TV
             ('tcl-tv', 'power', 'new_tv.svg'),
+            ('tcl-tv', 'home', 'home.svg'),
+            ('tcl-tv', 'netflix', 'netflix.svg'),
+        ],
+        [  # Row 2: Up
             ('tcl-tv', 'volume_up', 'volume_up.svg'), 
             ('tcl-tv', 'volume_down', 'volume_down.svg'),
+        ],
+        [  # Row 2: Up
+            ('tcl-tv', 'd-up', 'arrow_up.svg'), 
+        ],
+        [  # Row 2: Left/OK/Right
+            ('tcl-tv', 'd-left', 'arrow_left.svg'), 
+            ('tcl-tv', 'd-ok', 'arrow_ok.svg'), 
+            ('tcl-tv', 'd-right', 'arrow_right.svg'), 
+        ],
+        [  # Row 3: Down
+            ('tcl-tv', 'd-down', 'arrow_down.svg'), 
+        ]
+    ]
+    return render_template('pages/index.html', 
+            data={'all_devices': blink.get_device_dict(),
+                  'devices': devices, 
+                  })
             #('vizio-tv', 'power', 'new_tv.svg'),
             #('vizio-tv', 'volume_up', 'volume_up.svg'), 
             #('vizio-tv', 'volume_down', 'volume_down.svg'),
@@ -25,27 +46,13 @@ def home():
             #('sony-reciever', 'main_stb', 'computer.svg'),
             #('sony-reciever', 'main_game', 'ps4.svg'),
             #('sony-reciever', 'main_video', 'switch.svg'),
-        ],
-        [  # Section 1: Samsung TV
             #('samsung-tv', 'power', 'old_tv.svg'),
-            ('tcl-tv', 'Netflix', 'netflix.svg'),
             #('samsung-tv', 'volume_up', 'volume_up.svg'),
             #('samsung-tv', 'volume_down', 'volume_down.svg'),
             #('sony-reciever', 'zone2_sat-catv', 'chromecast.svg'),
             #('sony-reciever', 'zone2_stb', 'computer.svg'),
             #('sony-reciever', 'zone2_game', 'ps4.svg'),
             #('sony-reciever', 'zone2_video', 'switch.svg'),
-        ],
-        #[  # Section 1: Sony Receiver
-            #('sony-receiver', 'power', 'receiver.jpg'),
-            #('sony-receiver', 'volume_up', 'volume_up.svg'),
-            #('sony-receiver', 'volume_down', 'volume_down.svg'),
-        #]
-    ]
-    return render_template('pages/index.html', 
-            data={'all_devices': blink.get_device_dict(),
-                  'devices': devices, 
-                  })
 
 @app.route('/send/<string:device>/<string:command>')
 def send(device, command):
